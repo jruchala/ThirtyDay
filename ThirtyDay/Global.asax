c8dd3c785d6bd655444c604ba049bdb1da6fdcac
jruchala <jruchala@gmail.com>
2016-09-13 09:42:16 -0400

    Create New MVC project with one Controller and one View

---
+++ ThirtyDay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThirtyDay.MvcApplication" Language="C#" %>
---
+++ ThirtyDay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThirtyDay.Startup))]
+namespace ThirtyDay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
