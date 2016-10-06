99ffd570ab9ad2571269e1543c29257057f69c40
jruchala <jruchala@gmail.com>
2016-10-06 13:31:00 -0400

    day 11

---
+++ ThirtyDay/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Angela.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThirtyDay.Models;
+
+namespace ThirtyDay.Controllers
+{
+
+    public class PersonController : Controller
+    {
+        private static ICollection<Person> _people;
+        
+        static PersonController()
+        {
+            _people = Angie.Configure<Person>()
+                .Fill(p => p.BirthDate)
+                .AsPastDate()
+                .Fill(p => p.LikesMusic)
+                .WithRandom(new List<bool>() { true, true, true, true, false, false })
+                .Fill(p => p.Skills, () => new List<string>() { "Math", "Science", "History" })
+                .MakeList<Person>(20);
+        }
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(_people);
+        }
+    }
+}
