1848f65694e0d2c7b185614b73becf39d13db579
jruchala <jruchala@gmail.com>
2016-10-07 15:12:39 -0400

    Day 20 create a filter and add it to a controller action

---
+++ ThirtyDay/Filters/NotificationFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ThirtyDay.Models;
+
+namespace ThirtyDay.Filters
+{
+    public class NotificationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var context = new SiteDataContext();
+            var notifications = context.Notifications
+                .GroupBy(n => n.NotificationType)
+                .Select(g => new NotificationViewModel
+                {
+                    Count = g.Count(),
+                    NotificationType = g.Key.ToString(),
+                    BadgeClass = NotificationType.Email == g.Key
+                    ? "success"
+                    : "info"
+                });
+            filterContext.Controller.ViewBag.Notifications = notifications;
+        }
+    }
+}
